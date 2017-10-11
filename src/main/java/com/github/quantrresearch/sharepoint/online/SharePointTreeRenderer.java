ef43151b87eaa528178d6e9f87e5741d7f834ba7
Peter <peter@quantr.hk>
2017-10-12 01:21:19 +0800

    dev

--- src/main/java/com/github/quantrresearch/sharepoint/online/SharePointTreeRenderer.java
+++ src/main/java/com/github/quantrresearch/sharepoint/online/SharePointTreeRenderer.java
@@ -2,7 +2,6 @@ package com.github.quantrresearch.sharepoint.online;
 
 import java.awt.Color;
 import java.awt.Component;
-import javax.swing.BorderFactory;
 
 import javax.swing.JLabel;
 import javax.swing.JTree;
@@ -13,6 +12,8 @@ public class SharePointTreeRenderer extends JLabel implements TreeCellRenderer {
 
 	Color background = UIManager.getColor("Tree.background");
 	Color selectionBackground = UIManager.getColor("Tree.selectionBackground");
+	Color foreground = UIManager.getColor("Tree.foreground");
+	Color selectionForeground = UIManager.getColor("Tree.selectionForeground");
 
 	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
 		SharePointTreeNode node = (SharePointTreeNode) value;
@@ -20,14 +21,18 @@ public class SharePointTreeRenderer extends JLabel implements TreeCellRenderer {
 		this.setText(node.toString());
 		if (hasFocus) {
 			if (sel) {
+				this.setForeground(selectionForeground);
 				this.setBackground(selectionBackground);
 			} else {
+				this.setForeground(foreground);
 				this.setBackground(background);
 			}
 		} else {
 			if (sel) {
+				this.setForeground(foreground);
 				this.setBackground(Color.lightGray);
 			} else {
+				this.setForeground(foreground);
 				this.setBackground(background);
 			}
 		}
